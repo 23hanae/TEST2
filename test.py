526371fa4a5e0a7ea6b3c7faacb89bc0e579ede9
Hanae Atay <112653107+23hanae@users.noreply.github.com>
2022-09-07 11:01:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-import pygame
+class Arcenciel:
+    def __init__(self,couleurs):
+        self.couleurs = couleurs
+    
+    def Afficher4couleurs(self):
+        print("Les couleurs de l'arcenciel sont :" + str(self.couleurs))
+
+nb_couleurs = 6
+liste_couleurs = []
+for i in range(nb_couleurs):
+    couleurs = input("Les couleurs " + str(i+1) + " : ")
+    liste_couleurs.append(Arcenciel(couleurs))
+         
+for coleurs in liste_couleurs:
+    coleurs.Afficher4couleurs()
